4e59f8b4dc7b69d127efc45ab4d8952d0c21874a
Nazmul Alom <nazmulshuvo03@gmail.com>
2023-03-03 11:04:10 +0600

    nvim tre updated

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -11,6 +11,11 @@ vim.g.nvim_tree_highlight_opened_files = 1
 
 -- empty setup using defaults
 require("nvim-tree").setup({
+  view = {
+    cursorline = true,
+    number = true,
+    relativenumber = true,
+  },
   filters = {
       dotfiles = false,
     },
@@ -24,3 +29,15 @@ vim.keymap.set('n', '<leader>nn', ':NvimTreeFindFileToggle<CR>', {noremap = true
 vim.api.nvim_set_keymap('n', '<leader>nr', ':NvimTreeToggle<CR>', {noremap = true, silent = true}) -- open in current directory
 vim.api.nvim_set_keymap('n', '<leader>nf', ':NvimTreeFocus<CR>', {noremap = true, silent = true})
 vim.api.nvim_set_keymap('n', '<leader>ns', ':NvimTreeFindFile<CR>', {noremap = true, silent = true})
+
+local api = require('nvim-tree.api')
+local opts = function(desc)
+  return { desc = 'nvim-tree: ' .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+end
+
+vim.keymap.set('n', '<leader>nv', api.node.open.vertical, opts('Open: Vertical Split')) -- open file in vertical split
+vim.keymap.set('n', '<leader>ns', api.node.open.horizontal, opts('Open: Horizontal Split')) -- open file in horizontal split
+vim.keymap.set('n', '<leader>nt', api.node.open.tab, opts('Open: New Tab')) -- open file in new tab
+vim.keymap.set('n', '<leader>nt', api.node.open.tab, opts('Open: New Tab')) -- open file in new tab
+vim.keymap.set('n', '<leader>nj', 'gt')
+vim.keymap.set('n', '<leader>nk', 'gT')
