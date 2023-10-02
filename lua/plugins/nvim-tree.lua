457608c736d8de6f2b4c35da5c7e283e65e77ec8
Nazmul Alom <nazmulshuvo03@gmail.com>
2023-10-02 23:34:20 +0600

    nvim tree webicons setup updated

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -17,27 +17,33 @@ require("nvim-tree").setup({
     relativenumber = true,
   },
   filters = {
-      dotfiles = false,
-    },
+    dotfiles = false,
+  },
   git = {
-      enable = true,
-      ignore = false
-    },
+    enable = true,
+    ignore = false
+  },
 })
 
-vim.keymap.set('n', '<leader>nn', ':NvimTreeFindFileToggle<CR>', {noremap = true, silent = true}) -- open in current file directory
-vim.api.nvim_set_keymap('n', '<leader>nr', ':NvimTreeToggle<CR>', {noremap = true, silent = true}) -- open in current directory
-vim.api.nvim_set_keymap('n', '<leader>nf', ':NvimTreeFocus<CR>', {noremap = true, silent = true})
-vim.api.nvim_set_keymap('n', '<leader>ns', ':NvimTreeFindFile<CR>', {noremap = true, silent = true})
+require 'nvim-web-devicons'.setup {
+  color_icons = true,
+  default = true,
+  strict = true,
+}
+
+vim.keymap.set('n', '<leader>nn', ':NvimTreeFindFileToggle<CR>', { noremap = true, silent = true })  -- open in current file directory
+vim.api.nvim_set_keymap('n', '<leader>nr', ':NvimTreeToggle<CR>', { noremap = true, silent = true }) -- open in current directory
+vim.api.nvim_set_keymap('n', '<leader>nf', ':NvimTreeFocus<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>ns', ':NvimTreeFindFile<CR>', { noremap = true, silent = true })
 
 local api = require('nvim-tree.api')
 local opts = function(desc)
   return { desc = 'nvim-tree: ' .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
 end
 
-vim.keymap.set('n', '<leader>nv', api.node.open.vertical, opts('Open: Vertical Split')) -- open file in vertical split
+vim.keymap.set('n', '<leader>nv', api.node.open.vertical, opts('Open: Vertical Split'))     -- open file in vertical split
 vim.keymap.set('n', '<leader>ns', api.node.open.horizontal, opts('Open: Horizontal Split')) -- open file in horizontal split
-vim.keymap.set('n', '<leader>nt', api.node.open.tab, opts('Open: New Tab')) -- open file in new tab
-vim.keymap.set('n', '<leader>nt', api.node.open.tab, opts('Open: New Tab')) -- open file in new tab
+vim.keymap.set('n', '<leader>nt', api.node.open.tab, opts('Open: New Tab'))                 -- open file in new tab
+vim.keymap.set('n', '<leader>nt', api.node.open.tab, opts('Open: New Tab'))                 -- open file in new tab
 vim.keymap.set('n', '<leader>nj', 'gt')
 vim.keymap.set('n', '<leader>nk', 'gT')
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -2,7 +2,7 @@ require 'nvim-treesitter.configs'.setup {
   -- Add languages to be installed here that you want installed for treesitter
   ensure_installed = { 'c', 'cpp', 'lua', 'javascript', 'typescript', 'rust' },
 
-  highlight = { enable = true },
+  highlight = { enable = false },
   indent = { enable = true, disable = { 'python' } },
   incremental_selection = {
     enable = true,
