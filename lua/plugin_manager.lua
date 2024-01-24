f9e2e2fd25479fe8c907432132457cbb76e4e4db
Nazmul Alom <nazmulshuvo03@gmail.com>
2024-01-25 02:30:54 +0600

    telescope updated

--- lua/plugin_manager.lua
+++ lua/plugin_manager.lua
@@ -76,9 +76,11 @@ return require('packer').startup(function(use)
 
   -- Telescope
   use {
-    'nvim-telescope/telescope.nvim', tag = '0.1.1',
+    'nvim-telescope/telescope.nvim', tag = '0.1.5',
     requires = { { 'nvim-lua/plenary.nvim' } }
   }
+  use { 'nvim-telescope/telescope-fzf-native.nvim', run = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build' }
+
 
   -- Smooth navigation with tmux
   use 'alexghergh/nvim-tmux-navigation'
