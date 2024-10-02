48bd3dedc9a24d60cd95191fae0aae9f53decf3c
Nazmul Alom <nazmulshuvo03@gmail.com>
2024-10-02 22:57:08 +0600

    main file updated

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -9,18 +9,18 @@ vim.opt.rtp:prepend(vim.env.LAZY or lazypath)
 require("lazy").setup({
   spec = {
     -- add LazyVim and import its plugins
-    { "LazyVim/LazyVim", import = "lazyvim.plugins" },
+    {
+      "LazyVim/LazyVim",
+      import = "lazyvim.plugins",
+      opts = {
+        colorscheme = "default",
+      },
+    },
     -- import any extras modules here
     -- { import = "lazyvim.plugins.extras.lang.typescript" },
     -- { import = "lazyvim.plugins.extras.lang.json" },
     -- { import = "lazyvim.plugins.extras.ui.mini-animate" },
     -- import/override with your plugins
-    {
-      "LazyVim/LazyVim",
-      opts = {
-        colorscheme = "default",
-      },
-    },
     {
       "neovim/nvim-lspconfig",
       opts = {
@@ -38,7 +38,7 @@ require("lazy").setup({
     version = false, -- always use the latest git commit
     -- version = "*", -- try installing the latest stable version for plugins that support semver
   },
-  install = { colorscheme = { "tokyonight", "habamax" } },
+  -- install = { colorscheme = { "tokyonight", "habamax" } },
   checker = { enabled = true }, -- automatically check for plugin updates
   performance = {
     rtp = {
