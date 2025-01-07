911aff725844692bf50839d51a4f4e7684e6eeb2
Nazmul Alom <nazmulshuvo03@gmail.com>
2025-01-07 16:12:07 +0600

    python django plugins updated

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -27,6 +27,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.json" },
     { import = "lazyvim.plugins.extras.lang.markdown" },
     { import = "lazyvim.plugins.extras.formatting.prettier" },
+    { import = "lazyvim.plugins.extras.formatting.black" },
 
     -- import/override with your plugins
     {
